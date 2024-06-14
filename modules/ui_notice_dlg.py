375bfd3f6cde1b92b151c63570f94e1d3addcb45
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2024-06-14 10:47:38 +0900

    feat: notice dialog ui complete

--- modules/ui_notice_dlg.py
+++ modules/ui_notice_dlg.py
@@ -2,46 +2,22 @@ from PySide6.QtCore import *
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
 
-from . resources_rc import *
+from .resources_rc import *
+from .ui_dialog import Ui_Dialog  # 변환된 UI 파일을 임포트합니다.
 
-# 알림 버튼 클릭시 표출 다이얼로그
+# 빈 다이얼로그
 class CustomDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.ui = Ui_Dialog()  # UI 클래스 인스턴스를 생성합니다.
+        self.ui.setupUi(self)  # UI 설정을 다이얼로그에 적용합니다.
+        
         self.setWindowTitle("알림")
-        self.setGeometry(100, 100, 600, 400)
+        self.setGeometry(100, 100, 640, 470)
 
         self.setWindowIcon(QIcon(':/images/images/images/logo.png'))
 
-        # Set dialog background color using RGB
-        self.setStyleSheet("background-color: rgb(40, 44, 52);")  # 원하는 RGB 값으로 변경
-
-        # Dialog layout and widgets
-        layout = QVBoxLayout()  # 여기서 레이아웃을 먼저 선언합니다.
-        
-        self.label = QLabel("This is a custom notice dialog.")
-        layout.addWidget(self.label)
-
-        # QListView 설정
-        self.listView = QListView()
-        self.model = QStandardItemModel()
-        
-        # 리스트에 항목 추가
-        items = ["Item 1", "Item 2", "Item 3", "Item 4"]
-        for item in items:
-            listItem = QStandardItem(item)
-            self.model.appendRow(listItem)
-        
-        self.listView.setModel(self.model)
-        layout.addWidget(self.listView)
-        
-        self.okButton = QPushButton("나가기")
-        self.okButton.clicked.connect(self.accept)
-        layout.addWidget(self.okButton)
-        
-        self.setLayout(layout)  # 레이아웃을 다이얼로그에 설정합니다.
-        
-        # window 바로 앞에 위치 시킴
+        # 다이얼로그를 부모 창의 중앙에 위치시킴
         if parent:
             parent_rect = parent.geometry()
             self_rect = self.geometry()
